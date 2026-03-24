8a4022200a6130bcf64f45d2c907106c046b0071
Aztatzi03 <marco.perezazt@alumno.buap.mx>
2026-03-24 14:13:21 -0600

    solucion_anagrama

--- solver/exercise2/solutions/Solution_Marco_Antonio.py
+++ solver/exercise2/solutions/Solution_Marco_Antonio.py
@@ -2,9 +2,9 @@
 
 mi_lista = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
-anagrama1 = [print (x) for x in mi_lista if sorted(x.lower())==sorted("eat".lower())]
+anagrama1 = [print (x) for x in mi_lista if sorted(x)==sorted("eat".lower())]
 
-anagrama2 = [print (x) for x in mi_lista if sorted(x.lower())==sorted("tan".lower())]
+anagrama2 = [print (x) for x in mi_lista if sorted(x)==sorted("tan".lower())]
 
-anagrama3 = [print (x) for x in mi_lista if sorted(x.lower())==sorted("bat".lower())]
+anagrama3 = [print (x) for x in mi_lista if sorted(x)==sorted("bat".lower())]
 
