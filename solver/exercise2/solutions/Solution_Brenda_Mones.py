7edd124ec4005065773a31c30b46c829d5997eec
Brenda Mones <brendamones29@gmail.com>
2026-03-25 13:45:56 -0600

    casi pero no casi SIUUUU

--- solver/exercise2/solutions/Solution_Brenda_Mones.py
+++ solver/exercise2/solutions/Solution_Brenda_Mones.py
@@ -1,12 +1,29 @@
-def agrupar(lista):
-    grupos={}
-    for palabras in lista:
-        #separador.join(iterable)
-        # separador = ''.join(palabras)
-        separador = ''.join(sorted(palabras))
-        if separador not in grupos:
-                grupos[separador] = []
-        grupos[separador].append(palabras)
+# def agrupar(lista):
+#     grupos={}
+#     for palabras in lista:
+#         #separador.join(iterable)
+#         # separador = ''.join(palabras)
+#         separador = ''.join(sorted(palabras))
+#         if separador not in grupos:
+#                 grupos[separador] = []
+#         grupos[separador].append(palabras)
         
-    return list(grupos)
-#Aun no está xdxd
+#     return list(grupos)
+# #Aun no está xdxd
+def contar_letras(palabra):
+    conteo = {}
+    for letra in palabra:
+        if letra in conteo:
+            conteo[letra] += 1
+        else:
+            conteo[letra] = 1    
+    return conteo
+def anagramas(p1, p2):
+    return contar_letras(p1) == contar_letras(p2)
+def agrupar_anagramas(lista):
+    grupos = []
+    usados = [False] * len(lista)
+    return grupos
+
+letritas = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(agrupar_anagramas(letritas))
